06b648b430df8539683881e6c6e39a34e74d4e39
Unknownshangke <1009367153@qq.com>
2022-11-23 18:32:10 +0800

    Changes for datebases

--- src/main/java/com/xzp/forum/CampusForumApplication.java
+++ src/main/java/com/xzp/forum/CampusForumApplication.java
@@ -4,12 +4,14 @@ import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.github.pagehelper.PageHelper;
 
 @SpringBootApplication
-public class CampusForumApplication {
+public class CampusForumApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "false");
@@ -28,4 +30,9 @@ public class CampusForumApplication {
 		pageHelper.setProperties(p);
 		return pageHelper;
 	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CampusForumApplication.class);	}
 }
